ff79386a00846dff46954e104dbc9328463f0f81
Kevin Crawford <crawfordkevin101@gmail.com>
2021-06-06 17:20:28 -0400

    added questions array to script.js and added onclick functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,14 +19,35 @@ exit_btn.onclick = ()=>{
 continue_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide the info box
     quiz_box.classList.add("activeQuiz"); //show the quiz box
-    showQuestions();
+    showQuestions(0);
 }
 
 let que_count = 0;
 
-//getting questions and options from array
-function showQuestions(){
-    const que_text = document.querySelector(".que_text");
-    let que_tag = "<span>"+ questions[0].question +"</span>";
-    que_text.innerHTML = que_tag;
+const next_btn = document.querySelector(".next_btn")
+
+// If next button clicked
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){
+        que_count++;
+        showQuestions(que_count);
+    }else{
+        console.log("Questions completed")
+    }
+    
 }
+
+//getting questions and options from array
+function showQuestions(index){
+    const que_text = document.querySelector(".que_text");
+    const option_list = document.querySelector(".option_list");
+    let que_tag = "<span>"+ questions[index].numb + ". " + questions[index].question +"</span>";
+    let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+}
+
+const bottom_ques_counter = document.querySelector("");
